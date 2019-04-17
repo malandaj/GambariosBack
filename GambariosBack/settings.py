5b5eff65425f0888dc68b651964f3f1cafb30b46
mlanda <elmaildemlanda@gmail.com>
2019-04-16 20:20:21 -0500

    Se agrega biblioteca channels.

--- GambariosBack/settings.py
+++ GambariosBack/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Channels
+ASGI_APPLICATION = 'GambariosBack.routing.application'
